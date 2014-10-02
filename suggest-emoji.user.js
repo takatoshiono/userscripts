2d0cc132d52be8f98c9ca01ce2c48d1bb8bc9f65
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-10-03 00:06:54 +0900

    Add suggest-emoji.user.js

---
+++ suggest-emoji.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name          suggest-emoji
+// @namespace     https://github.com/takatoshiono
+// @description   suggest emoji from character input
+// @version       0.0.1
+// @author        Takatoshi Ono
+// @include       https://github.com/*
+// ==/UserScript==
+(function(){
+  console.log("hello userscript");
+})();
