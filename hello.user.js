9194aa10565e160b5017026cf6e4402d8e738381
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-10-02 23:38:50 +0900

    hello, world

---
+++ hello.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           test
+// @namespace      https://github.com/takatoshiono
+// @include        https://github.com/*
+// ==/UserScript==
+(function(){
+  console.log("hello userscript");
+})();
