83d18fea1d3663e60b99f768c4f530808373af14
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-10-02 23:48:43 +0900

    Add metadata

--- hello.user.js
+++ hello.user.js
@@ -1,7 +1,10 @@
 // ==UserScript==
-// @name           test
-// @namespace      https://github.com/takatoshiono
-// @include        https://github.com/*
+// @name          test
+// @namespace     https://github.com/takatoshiono
+// @description   test
+// @version       0.0.1
+// @author        Takatoshi Ono
+// @include       https://github.com/*
 // ==/UserScript==
 (function(){
   console.log("hello userscript");
