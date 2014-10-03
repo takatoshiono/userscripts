4c0ee2f9ee588c0b4291184d9620fd43b4749193
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-10-03 23:11:56 +0900

    Output keycode to console only when IME is on

--- suggest-emoji.user.js
+++ suggest-emoji.user.js
@@ -7,5 +7,16 @@
 // @include       https://github.com/*
 // ==/UserScript==
 (function(){
-  console.log("hello userscript");
+  console.log("hello, this is suggest-emoji");
+  var ime = false;
+  document.onkeydown = function(evt) {
+    // IME が on のときは keydown が 229 になる
+    if (evt.keyCode == 229) {
+      ime = true;
+    }
+  };
+  document.onkeyup = function(evt) {
+    if (ime == false) { return; }
+    console.log(evt);
+  };
 })();
